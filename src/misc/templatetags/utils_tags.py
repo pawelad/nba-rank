5cc5a7e1f4ca28db4571a55c0e1e17d3bea1649a
Paweł Adamczak <pawel.ad@gmail.com>
2016-03-05 19:15:21 +0100

    Added stats to vote view

--- src/misc/templatetags/utils_tags.py
+++ src/misc/templatetags/utils_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.filter
 def percentage(value):
     if value > 0.0:
-        return '{0:.1%}'.format(value)
+        return '{0:.1f}'.format(value * 100)
     else:
         return None
 
