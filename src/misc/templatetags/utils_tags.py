3c973addbb740572ed9820a4117753baae67a559
Paweł Adamczak <pawel.ad@gmail.com>
2016-03-13 04:08:37 +0100

    Added total votes to footer

--- src/misc/templatetags/utils_tags.py
+++ src/misc/templatetags/utils_tags.py
@@ -13,6 +13,16 @@ def percentage(value):
 
 @register.assignment_tag
 def get_version():
-    """Returns app version"""
+    """Return app version."""
     from nba_rank import __version__
     return __version__
+
+
+@register.assignment_tag
+def total_votes():
+    """Return the total number of votes."""
+    from django.db.models import Sum
+    from players.models import PlayerSeason
+    votes = PlayerSeason.objects.aggregate(win_votes=Sum('votes_win'),
+                                           tie_votes=Sum('votes_tie'))
+    return int(votes['win_votes'] + (votes['tie_votes'] / 2))
